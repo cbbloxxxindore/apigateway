490543c600ac2c5e5488f5607dbf59983e4ca2ee
abhijeet <abhijeet.thakur1@gmail.com>
2023-07-25 11:56:39 +0530

    added api gateway changes

---
+++ src/test/java/com/vanguard/apigatewatservice/ApigatewatserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vanguard.apigatewatservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigatewatserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
